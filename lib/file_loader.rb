fe4799d510ab2ae2298877ff0a541426df0c3849
Marko Zecevic <marczecevic@gmail.com>
2021-04-02 12:55:53 +0200

    Add count_ends method

--- lib/file_loader.rb
+++ lib/file_loader.rb
@@ -1,6 +1,8 @@
-class FileLoader
-    def initialize(file_name)
-        @file_name = file_name
-        @content = File.readLines(@file_name)
+class FileLoader < Linters
+    attr_reader :file_path, :content
+    def initialize(file_path)
+        @file_path = file_path
+        @file = file_path.to_s
+        @file_lines = File.readlines(@file)
     end
 end
--- lib/linters.rb
+++ lib/linters.rb
@@ -1,20 +1,44 @@
 require_relative 'file_loader.rb'
 class Linters 
 
-    def initialize(file_name)
-        @content = FileLoader.new(file_name)
+    attr_reader :content, :errors
+
+    def initialize(file_path)
+        @content = FileLoader.new(file_path)
         @errors = []
     end
 
-    
-    
     def trailing_white_spaces
+        
         @content.file_lines.each_with_index do |line,idx|
-            if line[-2] == ' '  @errors << "You have a trailing white space on line #{idx + 1}" 
+            if line[-2] == ' '  
+                @errors << "You have a trailing white space on line #{idx + 1}" 
             end
         end
     end
 
+    def count_ends
+        special_words = ['def','do','if']
+        count_start = 0
+        count_end = 0
+        @content.file_lines.each do |line|
+            if special_words.inlcude?(line.split(' '))
+                count_start += 1
+            elsif line.strip == 'end'
+                count_end += 1
+            end    
+        end 
+        if count_start < count_end
+            @errors << 'You have extra "end" somewhere'
+        elsif count_start > count_end
+            @errors << 'You are missing "end"'
+        end
+    end
+
 end 
+
+my_linter = Linters.new('test.rb')
+my_linter.trailing_white_spaces
+
 
 
