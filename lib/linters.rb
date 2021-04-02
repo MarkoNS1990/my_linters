5644e7239e373053c7e6ffca0bef5dff20ccf6a0
Marko Zecevic <marczecevic@gmail.com>
2021-04-02 13:26:15 +0200

    Change to loop over array instead of string

--- lib/linters.rb
+++ lib/linters.rb
@@ -2,7 +2,6 @@ class Linters
     attr_reader :file_path, :content, :file_lines, :errors
     def initialize(file_path)
         @file_path = file_path
-        @file = file_path.to_s
         @file_lines = File.readlines(@file)
         @errors = []
     end
