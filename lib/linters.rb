15c57e53112d5ed26f200b7280b989f7417b4d9b
Marko Zecevic <marczecevic@gmail.com>
2021-04-02 10:37:04 +0200

    Make trailing white_space method checker

--- lib/linters.rb
+++ lib/linters.rb
@@ -2,11 +2,19 @@ require_relative 'file_loader.rb'
 class Linters 
 
     def initialize(file_name)
-        @file = FileLoader.new(file_name)
+        @content = FileLoader.new(file_name)
         @errors = []
     end
 
     
+    
+    def trailing_white_spaces
+        @content.file_lines.each_with_index do |line,idx|
+            if line[-2] == ' '  @errors << "You have a trailing white space on line #{idx + 1}" 
+            end
+        end
+    end
+
 end 
 
 
